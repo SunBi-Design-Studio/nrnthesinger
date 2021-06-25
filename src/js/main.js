41e7c661f87fa803e1d89ce115cb55d853f8b91c
Anjit Pariyar <anjitpariyar@gmail.com>
2021-06-25 11:33:04 +0545

    responsive done

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,35 @@
 (function () {
   console.log("Have a good day 🎈");
 
-  console.log("▀▄▀▄▀▄▒G░u░l░p░░░S░t░a░r░t░e░r▒▄▀▄▀▄▀");
+  // opening a modal
+  const modalLink = document.querySelectorAll(".modal--link");
+  const backdrop = document.getElementById("modal--backdrop");
+  [...modalLink].forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      let modalTrigger = elem.getAttribute("data--modal");
+      console.log(modalTrigger);
+      document.getElementById(modalTrigger).classList.add("active");
+      backdrop.classList.add("active");
+    });
+  });
+
+  // closing a modal
+  const modals = document.querySelectorAll(".modal");
+  [...modals].forEach((elem) => {
+    elem.querySelector(".close").addEventListener("click", (e) => {
+      e.preventDefault();
+      elem.classList.remove("active");
+      backdrop.classList.remove("active");
+    });
+  });
+
+  // closing all modals
+  backdrop.addEventListener("click", (e) => {
+    e.preventDefault();
+    [...modals].forEach((elem, index) => {
+      elem.classList.remove("active");
+      backdrop.classList.remove("active");
+    });
+  });
 })();
